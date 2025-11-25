ba5a3b266d43bbdf59c3ed62cc97224b1e57bc18
Gabriel Thomas Tremblay <2330009@collegebart.ca>
2025-11-25 15:37:07 -0500

    Safe Zones and Innocent added

--- Hitman-master/Assets/Scripts/Enemy.cs
+++ Hitman-master/Assets/Scripts/Enemy.cs
@@ -5,11 +5,12 @@ public class Enemy : MonoBehaviour
 {
     private NavMeshAgent m_Agent;
     private Rigidbody m_Rigidbody;
+    private MeshRenderer m_Renderer;
     [SerializeField] private GameObject[] m_PatrolPoint;
     [SerializeField] private int m_currPoint;
     [SerializeField] public int state;
+    [SerializeField] private bool m_isGuard = true;
     private int health = 3;
-    private MeshRenderer m_Renderer;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -21,35 +22,60 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        switch (state)
+        if (m_isGuard)
         {
-            case 5: //Dead
-                m_Renderer.material.color = Color.black;
-                break;
-            case 1: //Alerter
-                break;
-            case 2: //Avertir
-                break;
-            case 3: //Ataquer
-                break;
-            case 4: //Fuir
-                break;
-            case 0: //Mort
-                if (m_Agent.remainingDistance <= m_Agent.stoppingDistance)
-                {
-                    if (m_currPoint + 2 <= m_PatrolPoint.Length)
+            switch (state)
+            {
+                case 5: //Dead
+                    m_Renderer.material.color = Color.black;
+                    break;
+                case 1: //Alerter
+                    break;
+                case 2: //Avertir
+                    break;
+                case 3: //Ataquer
+                    break;
+                case 4: //Fuir
+                    break;
+                case 0: //Idle
+                    if (m_Agent.remainingDistance <= m_Agent.stoppingDistance)
                     {
-                        m_currPoint++;
-                        m_Agent.SetDestination(m_PatrolPoint[m_currPoint].transform.position);
+                        if (m_currPoint + 2 <= m_PatrolPoint.Length)
+                        {
+                            m_currPoint++;
+                            m_Agent.SetDestination(m_PatrolPoint[m_currPoint].transform.position);
+                        }
+                        else if (m_PatrolPoint.Length > 0)
+                        {
+                            m_currPoint = 0;
+                            m_Agent.SetDestination(m_PatrolPoint[m_currPoint].transform.position);
+                        }
                     }
-                    else if(m_PatrolPoint.Length > 0)
-                    {
-                        m_currPoint = 0;
-                        m_Agent.SetDestination(m_PatrolPoint[m_currPoint].transform.position);
-                    }
-                }
-                break;
+                    break;
+            }
         }
+        else 
+        {
+            switch (state)
+            {
+                case 4: //Dead
+                    m_Renderer.material.color = Color.black;
+                    break;
+                case 1: //Alerter
+                    break;
+                case 2: //Fuir
+                    break;
+                case 3: //Rien
+                    break;
+                case 0: //Idle
+                    if (m_Agent.remainingDistance <= m_Agent.stoppingDistance && m_PatrolPoint.Length > 0)
+                    {
+                        m_Agent.SetDestination(m_PatrolPoint[Random.Range(0, m_PatrolPoint.Length)].transform.position);
+                    }
+                    break;
+            }
+        }
+
 
     }
 
---
+++ Hitman-master/Assets/Scripts/Innocent.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Innocent : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
